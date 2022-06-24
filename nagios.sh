df313a2b7a06eb37befb4c09d0b6abc131608b29
harshvyas23 <harshvyasm23@gmail.com>
2022-06-24 19:41:09 +0530

    Nagios_Script_till_9th_step

--- nagios.sh
+++ nagios.sh
@@ -38,3 +38,39 @@ sudo a2enmod cgi
 
 #you can restart the Apache Servers.
 sudo systemctl restart apache2
+
+#Install Nagios Plugins
+mydirPluginsTmp=/tmp/
+cd /tmp && wget https://nagios-plugins.org/download/nagios-plugins-2.3.3.tar.gz
+tar -zxvf /tmp/nagios-plugins-2.3.3.tar.gz
+dirPlugins=/tmp/nagios-plugins-2.3.3/
+cd /tmp/nagios-plugins-2.3.3/
+sudo ./configure --with-nagios-user=nagios --with-nagios-group=nagios
+sudo make
+sudo make install
+
+
+#Create Nagios service file
+echo "[Unit]" > /etc/systemd/system/nagios.service
+echo "Description=Nagios" >> /etc/systemd/system/nagios.service
+echo "BindTo=network.target" >> /etc/systemd/system/nagios.service
+echo "[Install]" >> /etc/systemd/system/nagios.service
+echo "WantedBy=multi-user.target" >> /etc/systemd/system/nagios.service
+echo "[Service]" >> /etc/systemd/system/nagios.service
+echo "User=nagios" >> /etc/systemd/system/nagios.service
+echo "Group=nagios" >> /etc/systemd/system/nagios.service
+echo "Type=simple" >> /etc/systemd/system/nagios.service
+echo "ExecStart=/usr/local/nagios/bin/nagios /usr/local/nagios/etc/nagios.cfg" >> /etc/systemd/system/nagios.service
+
+#Enable, Start and Restart the service
+sudo systemctl enable nagios.service 
+sudo systemctl start nagios.service
+sudo systemctl restart nagios.service
+
+#Allowed the permission to the new user
+sed -i 's/authorized_for_all_hosts=nagiosadmin/authorized_for_all_hosts=nagios/g' /usr/local/nagios/etc/cgi.cfg
+sed -i 's/authorized_for_all_services=nagiosadmin/authorized_for_all_services=nagios/g' /usr/local/nagios/etc/cgi.cfg
+sed -i 's/authorized_for_all_host_commands=nagiosadmin/authorized_for_all_host_commands=nagios/g' /usr/local/nagios/etc/cgi.cfg
+sed -i 's/authorized_for_all_service_commands=nagiosadmin/authorized_for_all_service_commands=nagios/g' /usr/local/nagios/etc/cgi.cfg
+sed -i 's/authorized_for_system_commands=nagiosadmin/authorized_for_system_commands=nagios/g' /usr/local/nagios/etc/cgi.cfg
+sed -i 's/authorized_for_configuration_information=nagiosadmin/authorized_for_configuration_information=nagios/g' /usr/local/nagios/etc/cgi.cfg
